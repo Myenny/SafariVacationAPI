1f7c1c2d4657014b9afa9d0cc387eb76f99e0988
ooeygooeygold <amichaelj1@gmail.com>
2018-11-13 13:02:28 -0500

    added search

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -1,16 +1,27 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Threading.Tasks;
-// using Microsoft.AspNetCore.Mvc;
-// using SafariVacationAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SafariVacationAPI;
+using SafariVacationAPI.Models;
 
-// namespace SafariVacationAPI.Controllers
-// {
-//   [Route("api/[controller]")]
-//   [ApiController]
-//   public class SafariVacationController : ControllerBase
-//   {
+namespace SafariApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class SearchController : ControllerBase
+  {
+    [HttpGet]
+    public IQueryable<SeenAnimals> Search(string searchTerm)
+    {
+      var db = new SafariVacationContext();
 
-//   }
-// }
+      var results = db.SeenAnimals.Where(w => w.Species.ToLower().Contains(searchTerm.ToLower()));
+
+      return results;
+      //https://localhost:5001/api/search?searchTerm=Li
+    }
+
+  }
+}
